514a810619e9ddb5db5862ef81a90711be1241af
Lukas Wutschitz <lw473@cam.ac.uk>
2018-02-15 14:05:17 +0000

    Use include guards

--- eigen-extra/include/PolynomialsDiff.hpp
+++ eigen-extra/include/PolynomialsDiff.hpp
@@ -1,34 +1,40 @@
+#ifndef EIGENEXTRA_POLYNOMIALSDIFF_HPP_
+#define EIGENEXTRA_POLYNOMIALSDIFF_HPP_
 
 #include <type_traits>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/Polynomials>
 
-template<typename Vec>
-struct IsDynamicVector : std::is_same<Vec,Eigen::Matrix<typename Vec::Scalar, Eigen::Dynamic, 1> >::type
-{};
-
-//! Compute the first derivative of a polynomial of a degree only known at runtime
-template<typename Polynomial>
-Polynomial poly_diff(const typename std::enable_if<IsDynamicVector<Polynomial>::value,Polynomial>::type& p)
+namespace EigenExtra
 {
-  Polynomial df;
-  df.resize(p.size()-1);
-  for(unsigned int i=0; i<df.size(); i++)
-  {
-    df[i] = p[i+1]*(i+1); 
-  }
-  return df;
+  template<typename Vec>
+    struct IsDynamicVector : std::is_same<Vec,Eigen::Matrix<typename Vec::Scalar, Eigen::Dynamic, 1> >::type
+  {};
+
+  //! Compute the first derivative of a polynomial of a degree only known at runtime
+  template<typename Polynomial>
+    Polynomial poly_diff(const typename std::enable_if<IsDynamicVector<Polynomial>::value,Polynomial>::type& p)
+    {
+      Polynomial df;
+      df.resize(p.size()-1);
+      for(unsigned int i=0; i<df.size(); i++)
+      {
+        df[i] = p[i+1]*(i+1); 
+      }
+      return df;
+    }
+
+  //! Compute the first derivative of a polynomial of a degree known at compile time
+  template<typename Polynomial>
+    Eigen::Matrix<typename Polynomial::Scalar,Polynomial::RowsAtCompileTime-1,1> poly_diff(const typename std::enable_if<!IsDynamicVector<Polynomial>::value,Polynomial>::type& p)
+    {
+      Eigen::Matrix<typename Polynomial::Scalar,Polynomial::RowsAtCompileTime-1,1> df;
+      for(unsigned int i=0; i<Polynomial::RowsAtCompileTime-1; i++)
+      {
+        df[i] = p[i+1]*(i+1); 
+      }
+      return df;
+    }
 }
 
-//! Compute the first derivative of a polynomial of a degree known at compile time
-template<typename Polynomial>
-Eigen::Matrix<typename Polynomial::Scalar,Polynomial::RowsAtCompileTime-1,1> poly_diff(const typename std::enable_if<!IsDynamicVector<Polynomial>::value,Polynomial>::type& p)
-{
-  Eigen::Matrix<typename Polynomial::Scalar,Polynomial::RowsAtCompileTime-1,1> df;
-  for(unsigned int i=0; i<Polynomial::RowsAtCompileTime-1; i++)
-  {
-    df[i] = p[i+1]*(i+1); 
-  }
-  return df;
-}
-
+#endif
